6c60e1599e67a5753fc319a2a8c6da62677234b7
Mehary G <mehary.aynealem1@gmail.com>
2021-09-06 23:28:22 -0500

    Some changes in the visual studio

--- Github/Class1.cs
+++ Github/Class1.cs
@@ -6,5 +6,6 @@ public class Class1
 	{
 		Console.CancelKeyPress();
 		Console.WriteLine("This is meant for the Practice of handling of conflicts")
+			Console.WriteLine("These changes are made on the visual studio")
 	}
 }

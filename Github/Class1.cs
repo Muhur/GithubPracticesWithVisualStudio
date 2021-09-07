f15bacede9c2be5dee94ccfeff4d3c7461a94a1c
Muhur <74337942+Muhur@users.noreply.github.com>
2021-09-06 23:25:35 -0500

    These updates were made on the git hub

--- Github/Class1.cs
+++ Github/Class1.cs
@@ -6,5 +6,9 @@ public class Class1
 	{
 		Console.CancelKeyPress();
 		Console.WriteLine("This is meant for the Practice of handling of conflicts")
+		Console.WriteLine("This is meant for the Practice of handling, this line is made for comparison purpose. I really like doing what I am doing")
+		Console.WriteLine("This is added on github by itself and hel for the comparison of these two things")
+		 
+
 	}
 }

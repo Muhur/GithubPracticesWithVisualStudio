593bc9d97e603146036a13dd824198c30a640941
Mehary G <mehary.aynealem1@gmail.com>
2021-09-12 22:18:09 -0500

    This is the change made in visual studio

--- Github/Class1.cs
+++ Github/Class1.cs
@@ -9,5 +9,8 @@ public class Class1
 		Console.WriteLine("These changes are made on the visual studio")
 		Console.WriteLine("This is meant for the Practice of handling, this line is made for comparison purpose. I really like doing what I am doing")
 		Console.WriteLine("This is added on github by itself and hel for the comparison of these two things")
+
+			//This is create a conflit and resolving a conflits
+		Consile.Write("We love this changes to make better, Changes made in the visual studio");
 	}
 }

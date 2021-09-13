0bdc82bc2898c7c34f4e2b761379c7eabf72bf71
Mehary G <mehary.aynealem1@gmail.com>
2021-09-12 22:06:46 -0500

    Afer resolving the conflits

--- Github/Class1.cs
+++ Github/Class1.cs
@@ -6,7 +6,7 @@ public class Class1
 	{
 		Console.CancelKeyPress();
 		Console.WriteLine("This is meant for the Practice of handling of conflicts")
-			Console.WriteLine("These changes are made on the visual studio")
+		Console.WriteLine("These changes are made on the visual studio")
 		Console.WriteLine("This is meant for the Practice of handling, this line is made for comparison purpose. I really like doing what I am doing")
 		Console.WriteLine("This is added on github by itself and hel for the comparison of these two things")
 	}
